0261ab38a369a9d080ecf40bcced9b1aa7c9571d
someone <98624654+AndyDoan1@users.noreply.github.com>
2022-03-03 12:53:59 -0500

    Updated Home.js
    Added Home.js

---
+++ Enviornments/main_project/frontend/src/Home.js
@@ -0,0 +1,152 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+export const themeOptions = {
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: 'rgba(100,255,218,0.82)',
+    },
+    secondary: {
+      main: '#00d9f5',
+    },
+    background: {
+      default: '#282c34',
+    },
+  },
+};
+
+const textPrimary = "#ffffff"
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        TuneIn.TV
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const cards = [1, 2, 3];
+
+const darkTheme = createTheme(themeOptions);
+
+export default function Home() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            TuneIn.TV
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="primary"
+              gutterBottom
+            >
+              What's happening right now:
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card variant="outlined"
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '0%',
+                    }}
+                    image="https://cdn.discordapp.com/attachments/919720082270203956/946920455942578267/unknown.png"
+                    alt="no"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2" color="primary">
+                      Title
+                    </Typography>
+                    <Typography color="primary">
+                      Stream details
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button color="primary" size="small" variant="contained">
+                      Join Stream
+                    </Button>
+                    <Button color="primary" size="small" variant="contained">
+                      Follow
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'rgba(100,255,218,0.82)', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Tunein.TV
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something something
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
