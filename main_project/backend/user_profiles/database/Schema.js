3fb9d3954e40e8b71dda06a08379b0111a9d2ba3
Crp28 <necrp28@gmail.com>
2022-03-13 05:05:57 -0400

    [INBIF-35] allows user to become streamer

--- main_project/backend/user_profiles/database/Schema.js
+++ main_project/backend/user_profiles/database/Schema.js
@@ -1,3 +1,4 @@
 let mongoose = require('mongoose');
 
 exports.User = mongoose.model('User', require('./UserSchema'));
+exports.StreamRoom = mongoose.model('StreamRoom', require('./StreamRoomSchema'));
