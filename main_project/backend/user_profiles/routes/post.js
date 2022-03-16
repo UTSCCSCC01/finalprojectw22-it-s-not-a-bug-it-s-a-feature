03912b579f172ccaa6dcfdb0673fc582e016bdc5
Eric Hua <erich333@hotmail.ca>
2022-03-16 15:17:41 -0400

    fixed disliking

--- main_project/backend/user_profiles/routes/post.js
+++ main_project/backend/user_profiles/routes/post.js
@@ -55,7 +55,7 @@ router.put("/:id/like", async (req, res) => {
       res.status(200).json("The post has been liked");
     } else {
       await post.updateOne({ $pull: { likes: req.body.userId } });
-      post.updateOne({ $inc: { likeCounter: -1 } });
+      await post.updateOne({ $inc: { likeCounter: -1 } });
       res.status(200).json("The post has been disliked");
     }
   } catch (err) {
