a39a13feb19ae60f56effd44d1cbe2208774166b
Eric Hua <erich333@hotmail.ca>
2022-04-01 19:00:52 -0400

    timeline bug hotfix

--- main_project/backend/livestream/backend/routes/post.js
+++ main_project/backend/livestream/backend/routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/Schema');
+const User = require('../models/UserSchema');
 const Post = require('../models/Schema').Post;
 
 
